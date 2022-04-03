4e4637a2ad359275e9884d71daad2d2ea233a60d
Takeru Ohta <phjgt308@gmail.com>
2022-04-03 13:30:38 +0900

    Use structs instead of maps

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::Parser;
 use erl_dist::term::{Atom, FixInteger, List, Map, Term};
-use std::collections::BTreeMap;
 use std::time::Duration;
 
 #[derive(Debug, Parser)]
@@ -32,10 +31,10 @@ fn main() -> anyhow::Result<()> {
 
         let mut msacc = Msacc::start(handle).await?;
         let data = msacc.get_stats().await?;
-        println!("{:?}", data);
 
         println!("System Realtime: {:?}", data.system_realtime());
         println!("System Runtime: {:?}", data.system_runtime());
+        println!("Type Stats: {:?}", data.type_stats());
 
         Ok(())
     })
@@ -106,11 +105,11 @@ impl Drop for Msacc {
 struct MsaccDataThread {
     id: i32,
     ty: MsaccType,
-    counters: BTreeMap<MsaccState, u64>,
+    counters: MsaccCounters,
 }
 
 impl MsaccDataThread {
-    fn from_term(term: Term) -> anyhow::Result<Self> {
+    fn from_term(term: Term, time_unit: u32) -> anyhow::Result<Self> {
         let mut map: Map = term.try_into().map_err(|_| anyhow::anyhow!("not a map"))?;
         let id: FixInteger = remove_map_entry(&mut map, "id")?;
 
@@ -119,42 +118,41 @@ impl MsaccDataThread {
             "scheduler" => MsaccType::Scheduler,
             "aux" => MsaccType::Aux,
             "async" => MsaccType::Async,
-            "dirty_cpu_scheduler" => MsaccType::DsirtyCpuScheduler,
-            "dirty_io_scheduler" => MsaccType::DsirtyIoScheduler,
+            "dirty_cpu_scheduler" => MsaccType::DirtyCpuScheduler,
+            "dirty_io_scheduler" => MsaccType::DirtyIoScheduler,
             "poll" => MsaccType::Poll,
             ty => anyhow::bail!("unknown msacc type {:?}", ty),
         };
 
         let msacc_data_counters: Map = remove_map_entry(&mut map, "counters")?;
-        let mut counters = BTreeMap::new();
+        let mut counters = MsaccCounters::default();
         for (k, v) in msacc_data_counters.entries {
-            let k: Atom = k.try_into().map_err(|_| anyhow::anyhow!("not an atom"))?;
-            let k = match k.name.as_str() {
-                "alloc" => MsaccState::Alloc,
-                "aux" => MsaccState::Aux,
-                "bif" => MsaccState::Bif,
-                "busy_wait" => MsaccState::BusyWait,
-                "check_io" => MsaccState::CheckIo,
-                "emulator" => MsaccState::Emulator,
-                "ets" => MsaccState::Ets,
-                "gc" => MsaccState::Gc,
-                "gc_fullsweep" => MsaccState::GcFullsweep,
-                "nif" => MsaccState::Nif,
-                "other" => MsaccState::Other,
-                "port" => MsaccState::Port,
-                "send" => MsaccState::Send,
-                "sleep" => MsaccState::Sleep,
-                "timers" => MsaccState::Timers,
-                k => anyhow::bail!("unknown msacc state {:?}", k),
-            };
-
-            let v = match v {
+            let v: u64 = match v {
                 Term::FixInteger(v) => v.value.try_into()?,
                 Term::BigInteger(v) => v.value.try_into()?,
                 _ => anyhow::bail!("not an integer"),
             };
+            let v = Duration::from_secs_f64(v as f64 / time_unit as f64);
 
-            counters.insert(k, v);
+            let k: Atom = k.try_into().map_err(|_| anyhow::anyhow!("not an atom"))?;
+            match k.name.as_str() {
+                "alloc" => counters.alloc += v,
+                "aux" => counters.aux += v,
+                "bif" => counters.bif += v,
+                "busy_wait" => counters.busy_wait += v,
+                "check_io" => counters.check_io += v,
+                "emulator" => counters.emulator += v,
+                "ets" => counters.ets += v,
+                "gc" => counters.gc += v,
+                "gc_fullsweep" => counters.gc_fullsweep += v,
+                "nif" => counters.nif += v,
+                "other" => counters.other += v,
+                "port" => counters.port += v,
+                "send" => counters.send += v,
+                "sleep" => counters.sleep += v,
+                "timers" => counters.timers += v,
+                k => anyhow::bail!("unknown msacc state {:?}", k),
+            }
         }
         Ok(Self {
             id: id.value,
@@ -164,39 +162,103 @@ impl MsaccDataThread {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
-enum MsaccState {
-    Alloc,
-    Aux,
-    Bif,
-    BusyWait,
-    CheckIo,
-    Emulator,
-    Ets,
-    Gc,
-    GcFullsweep,
-    Nif,
-    Other,
-    Port,
-    Send,
-    Sleep,
-    Timers,
+#[derive(Debug, Clone, Default)]
+struct MsaccCounters {
+    alloc: Duration,
+    aux: Duration,
+    bif: Duration,
+    busy_wait: Duration,
+    check_io: Duration,
+    emulator: Duration,
+    ets: Duration,
+    gc: Duration,
+    gc_fullsweep: Duration,
+    nif: Duration,
+    other: Duration,
+    port: Duration,
+    send: Duration,
+    sleep: Duration,
+    timers: Duration,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+impl MsaccCounters {
+    fn realtime_sum(&self) -> Duration {
+        self.alloc
+            + self.aux
+            + self.bif
+            + self.busy_wait
+            + self.check_io
+            + self.emulator
+            + self.ets
+            + self.gc
+            + self.gc_fullsweep
+            + self.nif
+            + self.other
+            + self.port
+            + self.send
+            + self.sleep
+            + self.timers
+    }
+
+    fn runtime_sum(&self) -> Duration {
+        self.alloc
+            + self.aux
+            + self.bif
+            + self.busy_wait
+            + self.check_io
+            + self.emulator
+            + self.ets
+            + self.gc
+            + self.gc_fullsweep
+            + self.nif
+            + self.other
+            + self.port
+            + self.send
+            + self.timers
+    }
+
+    fn add_assign(&mut self, other: &Self) {
+        self.alloc += other.alloc;
+        self.aux += other.aux;
+        self.bif += other.bif;
+        self.busy_wait += other.busy_wait;
+        self.check_io += other.check_io;
+        self.emulator += other.emulator;
+        self.ets += other.ets;
+        self.gc += other.gc;
+        self.gc_fullsweep += other.gc_fullsweep;
+        self.nif += other.nif;
+        self.other += other.other;
+        self.port += other.port;
+        self.send += other.send;
+        self.sleep += other.sleep;
+        self.timers += other.timers;
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 enum MsaccType {
     Scheduler,
     Aux,
     Async,
-    DsirtyCpuScheduler,
-    DsirtyIoScheduler,
+    DirtyCpuScheduler,
+    DirtyIoScheduler,
     Poll,
+}
+
+#[derive(Debug, Clone, Default)]
+struct MsaccTypeStats {
+    scheduler: MsaccCounters,
+    aux: MsaccCounters,
+    r#async: MsaccCounters,
+    dirty_cpu_scheduler: MsaccCounters,
+    dirty_io_scheduler: MsaccCounters,
+    poll: MsaccCounters,
 }
 
 #[derive(Debug)]
 struct MsaccData {
     pub threads: Vec<MsaccDataThread>,
-    pub time_unit: u32,
 }
 
 impl MsaccData {
@@ -204,37 +266,36 @@ impl MsaccData {
         let list: List = term.try_into().map_err(|_| anyhow::anyhow!("not a list"))?;
         let mut threads = Vec::new();
         for msacc_data_thread in list.elements {
-            threads.push(MsaccDataThread::from_term(msacc_data_thread)?);
+            threads.push(MsaccDataThread::from_term(msacc_data_thread, time_unit)?);
         }
-        Ok(Self { threads, time_unit })
-    }
-
-    fn to_duration(&self, t: u64) -> Duration {
-        Duration::from_secs_f64(t as f64 / self.time_unit as f64)
+        Ok(Self { threads })
     }
 
     fn system_realtime(&self) -> Duration {
-        let t = self
-            .threads
-            .iter()
-            .map(|t| t.counters.values().copied().sum::<u64>())
-            .sum();
-        self.to_duration(t)
+        self.threads.iter().map(|t| t.counters.realtime_sum()).sum()
     }
 
     fn system_runtime(&self) -> Duration {
-        let t = self
-            .threads
-            .iter()
-            .map(|t| {
-                t.counters
-                    .iter()
-                    .filter(|x| *x.0 != MsaccState::Sleep)
-                    .map(|x| x.1)
-                    .sum::<u64>()
-            })
-            .sum();
-        self.to_duration(t)
+        self.threads.iter().map(|t| t.counters.runtime_sum()).sum()
+    }
+
+    fn type_stats(&self) -> MsaccTypeStats {
+        let mut stats = MsaccTypeStats::default();
+        for thread in &self.threads {
+            match thread.ty {
+                MsaccType::Scheduler => stats.scheduler.add_assign(&thread.counters),
+                MsaccType::Async => stats.r#async.add_assign(&thread.counters),
+                MsaccType::Aux => stats.aux.add_assign(&thread.counters),
+                MsaccType::DirtyCpuScheduler => {
+                    stats.dirty_cpu_scheduler.add_assign(&thread.counters)
+                }
+                MsaccType::DirtyIoScheduler => {
+                    stats.dirty_io_scheduler.add_assign(&thread.counters)
+                }
+                MsaccType::Poll => stats.poll.add_assign(&thread.counters),
+            }
+        }
+        stats
     }
 }
 
