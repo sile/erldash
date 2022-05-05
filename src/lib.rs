35b8873088db0c7b718c774ef092a7d18bc97fad
Takeru Ohta <phjgt308@gmail.com>
2022-05-05 15:23:18 +0900

    Refactor lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
+//! A simple, terminal-based Erlang dashboard.
 pub mod erlang;
 pub mod metrics;
 pub mod ui;
 
 #[derive(Debug, Clone, clap::Parser)]
 pub struct Options {
+    /// Target Erlang node name.
     pub erlang_node: erl_dist::node::NodeName,
 
+    /// Erlang metrics polling interval (in seconds).
     #[clap(long, short = 'i', default_value = "1")]
     pub polling_interval: std::num::NonZeroUsize,
 
+    /// Erlang cookie.
+    ///
+    /// By default, the content of the `$HOME/.erlang.cookie` file is used.
     #[clap(long, short = 'c')]
     pub cookie: Option<String>,
 }
